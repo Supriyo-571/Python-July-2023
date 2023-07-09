a89c2250590cf051d635fa334902e856df99ee80
Supriyo-571 <131594302+Supriyo-571@users.noreply.github.com>
2023-07-09 21:40:55 +0530

    Update arrays.py

--- PYTHON MSIT/arrays.py
+++ PYTHON MSIT/arrays.py
@@ -2,7 +2,7 @@ class Arrays:
     def array_types():
         arr_1=[1,2,3,4,5,"test",23.36,False,23+4j]
         arr_2=(1,2,3)
-        arr_3={"mainak",123,569}
+        arr_3={"supriyo",123,569}
         arr_4={
             "name" : "google",
             "age":"1000",
